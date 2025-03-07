84208f6d9669e8549e55a9aa1378a3f661e8c6db
BayeDiagn <135014924+BayeDiagn@users.noreply.github.com>
2025-03-07 16:45:17 +0000

    ajout de  @ApiResponses dans le controller

--- gestion_praticiens/src/main/java/com/gestion/praticiens/gestion_praticiens/controller/PraticienController.java
+++ gestion_praticiens/src/main/java/com/gestion/praticiens/gestion_praticiens/controller/PraticienController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gestion.praticiens.gestion_praticiens.model.Praticien;
 import com.gestion.praticiens.gestion_praticiens.service.PraticienService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1")
 public class PraticienController {
 
@@ -30,31 +36,59 @@ public class PraticienController {
         this.praticienService = praticienService;
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+    // Rechercher tous les praticiens
+    @Operation(summary = "Rechercher tous les praticiens", description = "Rechercher tous les praticiens")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Praticiens trouvés", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Praticien.class))),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content)
+    })
     @GetMapping("/praticiens")
     public ResponseEntity<List<Praticien>> getPraticiens() {
         return ResponseEntity.ok(praticienService.recupererPraticiens());
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+    // Rechercher un praticien
+    @Operation(summary = "Rechercher un praticien", description = "Rechercher un praticien")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Praticien trouvé", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Praticien.class))),
+            @ApiResponse(responseCode = "404", description = "Praticien non trouvé", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content)
+    })
     @GetMapping("/praticien/{id}")
     public ResponseEntity<Praticien> getPraticienById(@PathVariable String id) {
         return ResponseEntity.ok(praticienService.recupererPraticien(id));
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+    // Créer un nouveau praticien
+    @Operation(summary = "Créer un nouveau praticien", description = "Ajoute un nouveau praticien au système")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Praticien créé avec succès", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Praticien.class))),
+            @ApiResponse(responseCode = "400", description = "Données d'entrée invalides", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content)
+    })
     @PostMapping("/creer-praticien")
     public ResponseEntity<Praticien> createPraticien(@Valid @RequestBody Praticien praticien) {
         return ResponseEntity.ok(praticienService.creerPraticien(praticien));
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+    // Modifier un praticien
+    @Operation(summary = "Modifier un praticien", description = "Modifier un praticien")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Praticien modifié", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Praticien.class))),
+            @ApiResponse(responseCode = "400", description = "Données d'entrée invalides", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Praticien non trouvé", content = @Content),
+    })
     @PutMapping("/modifier-praticien/{id}")
     public ResponseEntity<Praticien> updatePraticien(@PathVariable String id, @Valid @RequestBody Praticien praticien) {
         return ResponseEntity.ok(praticienService.modifierPraticien(id, praticien));
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+    // Supprimer un praticien
+    @Operation(summary = "Supprimer un praticien", description = "Supprimer un praticien")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Praticien supprimé", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Praticien.class))),
+            @ApiResponse(responseCode = "404", description = "Praticien non trouvé", content = @Content),
+    })
     @DeleteMapping("/supprimer-praticien/{id}")
     public ResponseEntity<Map<String, String>> deletePraticien(@PathVariable String id) {
         return ResponseEntity.ok(praticienService.supprimerPraticien(id));
